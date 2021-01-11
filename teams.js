903faa89805742cc924f66962673c0c74a71388d
jequezinho <jroverby92@gmail.com>
2021-01-10 19:15:03 -0600

    First commit! With teams Object

---
+++ teams.js
@@ -0,0 +1,88 @@
+const teams = {
+    'kc': {
+        'name': 'Kansas City Chiefs',
+        'conference': 'AFC',
+        'ELO': 1722,
+        'seed': 1
+    }, 'buf':
+    {
+        'name': 'Buffalo Bills',
+        'conference': 'AFC',
+        'ELO': 1725,
+        'seed': 2
+    }, 'pit':
+    {
+        'name': 'Pittsburgh Steelers',
+        'conference': 'AFC',
+        'ELO': 1587,
+        'seed': 3
+    }, 'ten':
+    {
+        'name': 'Tennessee Titans',
+        'conference': 'AFC',
+        'ELO': 1585,
+        'seed': 4
+    }, 'bal':
+    {
+        'name': 'Baltimore Ravens',
+        'conference': 'AFC',
+        'ELO': 1725,
+        'seed': 5
+    }, 'cle':
+    {
+        'name': 'Cleveland Browns',
+        'conference': 'AFC',
+        'ELO': 1550,
+        'seed': 6
+    }, 'ind':
+    {
+        'name': 'Indianapolis Colts',
+        'conference': 'AFC',
+        'ELO': 1597,
+        'seed': 7
+    }, 'gb':
+    {
+        'name': 'Green Bay Packers',
+        'conference': 'NFC',
+        'ELO': 1703,
+        'seed': 1
+    }, 'no':
+    {
+        'name': 'New Orleans Saints',
+        'conference': 'NFC',
+        'ELO': 1730,
+        'seed': 2
+    }, 'sea':
+    {
+        'name': 'Seattle Seahawks',
+        'conference': 'NFC',
+        'ELO': 1615,
+        'seed': 3
+    }, 'was':
+    {
+        'name': 'Washington Redskins',
+        'conference': 'NFC',
+        'ELO': 1466,
+        'seed': 4
+    }, 'tb':
+    {
+        'name': 'Tampa Bay Buccaneers',
+        'conference': 'NFC',
+        'ELO': 1642,
+        'seed': 5
+    }, 'lar':
+    {
+        'name': 'Los Angeles Rams',
+        'conference': 'NFC',
+        'ELO': 1586,
+        'seed': 6
+    }, 'chi':
+    {
+        'name': 'Chicago Bears',
+        'conference': 'NFC',
+        'ELO': 1515,
+        'seed': 7
+    }
+}
+
+export default teams;
